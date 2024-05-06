73550087804c4267220251aa025edd207f7cefc5
DoOoMB <goworuha.egor@yandex.ru>
2024-05-06 10:01:24 +0300

    0.1 release

---
+++ Assets/Scripts/Tank/TankMovement.cs
@@ -0,0 +1,136 @@
+﻿using UnityEngine;
+
+
+public class TankMovement : MonoBehaviour
+{
+    public int m_PlayerNumber = 1;
+    public float m_Speed = 12f;
+    public float m_TurnSpeed = 180f;
+    public float m_TurretTurnSpeed = 60f;
+    public AudioSource m_MovementAudio;
+    public AudioClip m_EngineIdling;
+    public AudioClip m_EngineDriving;
+	public float m_PitchRange = 0.2f;
+
+    private string m_MovementAxisName;
+    private string m_TurnAxisName;
+    private string m_TurretTurnAxisName;
+    private Rigidbody m_Rigidbody;
+    private float m_MovementInputValue;
+    private float m_TurnInputValue;             
+    private float m_TurretTurnInputValue;
+    private float m_OriginalPitch;
+    private ParticleSystem[] m_particleSystems;
+    private Transform m_TurretTurnAxis;
+
+
+    private void Awake ()
+    {
+        m_Rigidbody = GetComponent<Rigidbody> ();
+        m_TurretTurnAxis = transform.Find("TankRenderers/TurretRotationAxis");
+    }
+
+
+    private void OnEnable ()
+    {
+        m_Rigidbody.isKinematic = false;
+
+        m_MovementInputValue = 0f;
+        m_TurnInputValue = 0f;
+
+        m_particleSystems = GetComponentsInChildren<ParticleSystem>();
+        for (int i = 0; i < m_particleSystems.Length; ++i)
+        {
+            m_particleSystems[i].Play();
+        }
+    }
+
+
+    private void OnDisable ()
+    {
+        m_Rigidbody.isKinematic = true;
+
+        for(int i = 0; i < m_particleSystems.Length; ++i)
+        {
+            m_particleSystems[i].Stop();
+        }
+    }
+
+
+    private void Start ()
+    {
+        m_MovementAxisName = "Vertical" + m_PlayerNumber;
+        m_TurnAxisName = "Horizontal" + m_PlayerNumber;
+        m_TurretTurnAxisName = "HorizontalT" + m_PlayerNumber;
+
+        m_OriginalPitch = m_MovementAudio.pitch;
+    }
+
+
+    private void Update ()
+    {
+        m_MovementInputValue = Input.GetAxis (m_MovementAxisName);
+        m_TurnInputValue = Input.GetAxis (m_TurnAxisName);
+        m_TurretTurnInputValue = Input.GetAxis(m_TurretTurnAxisName);
+        EngineAudio ();
+    }
+
+
+    private void EngineAudio ()
+    {
+        if (Mathf.Abs (m_MovementInputValue) < 0.1f && Mathf.Abs (m_TurnInputValue) < 0.1f)
+        {
+            if (m_MovementAudio.clip == m_EngineDriving)
+            {
+                m_MovementAudio.clip = m_EngineIdling;
+                m_MovementAudio.pitch = Random.Range (m_OriginalPitch - m_PitchRange, m_OriginalPitch + m_PitchRange);
+                m_MovementAudio.Play ();
+            }
+        }
+        else
+        {
+            if (m_MovementAudio.clip == m_EngineIdling)
+            {
+                m_MovementAudio.clip = m_EngineDriving;
+                m_MovementAudio.pitch = Random.Range(m_OriginalPitch - m_PitchRange, m_OriginalPitch + m_PitchRange);
+                m_MovementAudio.Play();
+            }
+        }
+    }
+
+
+    private void FixedUpdate ()
+    {
+        Move ();
+        Turn ();
+        TurnTurret();
+    }
+
+
+    private void Move ()
+    {
+        Vector3 movement = transform.forward * m_MovementInputValue * m_Speed * Time.deltaTime;
+
+        m_Rigidbody.MovePosition(m_Rigidbody.position + movement);
+    }
+
+
+    private void Turn ()
+    {
+        float turn = m_TurnInputValue * m_TurnSpeed * Time.deltaTime;
+
+        Quaternion turnRotation = Quaternion.Euler (0f, turn, 0f);
+
+        m_Rigidbody.MoveRotation (m_Rigidbody.rotation * turnRotation);
+        m_TurretTurnAxis.rotation = turnRotation * m_TurretTurnAxis.rotation;
+    }
+
+    private void TurnTurret()
+    {
+        float turn = m_TurretTurnInputValue * m_TurretTurnSpeed * Time.deltaTime;
+
+        Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
+
+        m_TurretTurnAxis.rotation = turnRotation * m_TurretTurnAxis.rotation;
+    }
+}
---
+++ Assets/Scripts/Tank/TankShooting.cs
@@ -0,0 +1,60 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class TankShooting : MonoBehaviour
+{
+    public int m_PlayerNumber = 1;              
+    public GameObject m_ShellExplosionPrefab;                   
+    public Transform m_FireTransform;           
+    public AudioSource m_ShootingAudio;         
+    public AudioClip m_ChargingClip;            
+    public AudioClip m_FireClip;                    
+
+    public float m_ReloadCooldown = 2f;
+    private float m_CurrentCooldownTime = 2f;
+    private string m_FireButton;                
+
+    private void Start ()
+    {
+        // The fire axis is based on the player number.
+        m_FireButton = "Fire" + m_PlayerNumber;
+
+    }
+
+
+    private void Update ()
+    {
+        if (Input.GetButtonDown(m_FireButton) && m_CurrentCooldownTime >= m_ReloadCooldown)
+        {
+            Fire();
+            m_ShootingAudio.clip = m_ChargingClip;
+            m_ShootingAudio.Play ();
+            m_CurrentCooldownTime = 0f;
+        }
+        if (m_ReloadCooldown >= m_CurrentCooldownTime) m_CurrentCooldownTime += Time.deltaTime;
+    }
+
+
+    private void Fire ()
+    {
+        m_ShootingAudio.clip = m_FireClip;
+        m_ShootingAudio.Play();
+        Ray ray = new Ray(m_FireTransform.position, m_FireTransform.forward);
+        if (Physics.Raycast(ray, out RaycastHit Hit))
+        {
+            GameObject explosionInstance = 
+                Instantiate(m_ShellExplosionPrefab, Hit.point, m_FireTransform.rotation);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Ray ray = new Ray(transform.position, m_FireTransform.forward);
+        Gizmos.color = Color.green;
+        Physics.Raycast(ray, out RaycastHit Hit);
+        Debug.DrawRay(ray.origin, ray.direction, Color.green);
+        Gizmos.DrawSphere(Hit.point, 1f);
+    }
+}
